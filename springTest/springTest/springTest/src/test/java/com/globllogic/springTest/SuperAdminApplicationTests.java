6b1d9b91627727dcaa2cfe9247882fc9e04d6f1e
Agam Mishra <agam.mishra@globallogic.com>
2022-08-16 14:23:55 +0530

    Training

---
+++ springTest/springTest/springTest/src/test/java/com/globllogic/springTest/SuperAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globllogic.springTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
