c56a74f5ed63d6a9046918f11bbd85a303817893
Agam Mishra <agam.mishra@globallogic.com>
2022-08-22 17:10:30 +0530

    Training

--- OnlineBook/OnlineBook/src/main/java/com/globallogic/BookStore/repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.globallogic.BookStore.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.globallogic.BookStore.entity.user;
-
-public interface UserRepo extends JpaRepository<user,Long> {
-
-}
